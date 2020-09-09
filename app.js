b1c95440b953a392d98fff237d52189ffa07ec70
Paulaskub <poli.skubic@gmail.com>
2020-09-08 21:35:51 -0300

    Primer intento de solucion de Digital-heroes

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const app = express()
+const path = require("path")
+
+
+app.get(`/`, function(req,res) { 
+
+    let texto = `Ni Superman, Iron Man o La Mujer Maravilla son tan importantes cómo las y los Heroes de carne y
+    hueso que encontrarás en este sitio. Esperamos que ellas y ellos te sirvan como
+    inspiración para poder cumplir tus objetivos. Recuerda: ¡nunca pares de creer en
+    ti!`
+
+    res.send(texto)
+ })
+
+ app.get(`/heroes`, function(req,res) { 
+
+    let texto = path.resolve('heroes.json')
+
+    res.sendFile(texto)
+ })
+
+ app.listen(3000)
